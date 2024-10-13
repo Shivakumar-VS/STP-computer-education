3fd4f0569755aa223bb7700980a473da61bfc6d8
Shivakumar-001 <shivagana2004@gmail.com>
2024-10-13 23:33:06 +0530

    ADDSUBMULDIV.js

---
+++ addSumMudiv.js
@@ -0,0 +1,23 @@
+// Addition
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log("Addition: " + num1 + " + " + num2 + " = " + sum);
+
+// Deletion (Removing an element from an array)
+let array = [1, 2, 3, 4, 5];
+let indexToDelete = 2; // Removing the element at index 2 (value 3)
+array.splice(indexToDelete, 1);
+console.log("Array after deletion: " + array);
+
+// Multiplication
+let num3 = 6;
+let num4 = 4;
+let product = num3 * num4;
+console.log("Multiplication: " + num3 + " * " + num4 + " = " + product);
+
+// Division
+let num5 = 20;
+let num6 = 4;
+let quotient = num5 / num6;
+console.log("Division: " + num5 + " / " + num6 + " = " + quotient);
