dc8e432046d092dac4c20dc2b5fc46de26275d89
Shivakumar-001 <shivagana2004@gmail.com>
2024-12-09 20:51:32 +0530

    switch statement

---
+++ MDN Docs/switch.js
@@ -0,0 +1,6 @@
+const expr = 'papaya';
+switch(expr){
+    case 'oranges': console.log("oranges are 50rupees per kg");break;
+    case 'mango'  : console.log("mango are 120rupees per kg");break;
+    case 'papaya' : console.log("papayas are 90rupees per kg");break;
+}
