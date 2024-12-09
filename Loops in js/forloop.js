1df58bc7e540dcaa5bb105927a187db2597a0dfc
Shivakumar-001 <shivagana2004@gmail.com>
2024-12-09 21:24:57 +0530

    for loop

---
+++ Loops in js/forloop.js
@@ -0,0 +1,10 @@
+//Loops are used to execute a piece of code again and again
+
+/* syntax :    for(let i=1;i<=5; i++)
+                {
+                        //console.log("apna college");
+                }
+*/
+for(let i=1; i<=100;i++){
+    console.log("kateel Ashok pai memorial college");
+}
