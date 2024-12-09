6e0e3d498fabd56c10bacb082c7ea02514b7c8ca
Shivakumar-001 <shivagana2004@gmail.com>
2024-12-09 21:25:58 +0530

    Adding comment

--- Loops in js/forloop.js
+++ Loops in js/forloop.js
@@ -8,3 +8,10 @@
 for(let i=1; i<=100;i++){
     console.log("kateel Ashok pai memorial college");
 }
+//sum of numbers
+let sum = 0;
+for(let i=1;i<=5;i++){
+    sum = sum+i;
+}
+console.log("sum =",sum);
+//output =15;
