fd5e7506e9b00ecccd635cced5d1652195f79b55
Shivakumar-001 <shivagana2004@gmail.com>
2024-12-11 12:13:52 +0530

    Arrays in Js

---
+++ Arrays in JS/array.js
@@ -0,0 +1,19 @@
+//collection of multiple items 
+let heroes = ["ironMan","hulk","batman","shaktiman","antman","thor"];
+for(let i= 0; i<heroes.length;i++)
+{
+    console.log(heroes[i]);
+}
+
+// or we can use for-of loop
+for(let el of heroes)
+{
+    console.log(el);
+}
+
+//cities values
+let cities = ["mumbai","bengaluru","delhi"];
+for(let city of cities)
+{
+    console.log(city.toUpperCase);
+}
