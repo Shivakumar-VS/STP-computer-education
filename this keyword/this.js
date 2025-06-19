6a69c8a4437997accff215fd2f6cce798fdb4802
Shivakumar-001 <shivagana2004@gmail.com>
2025-06-19 22:40:19 +0530

    This keyword in JS

---
+++ this keyword/this.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+// Display data from the object:
+document.getElementById("demo").innerHTML = person.fullName();
+
+
+//OUTPUT : John Doe
